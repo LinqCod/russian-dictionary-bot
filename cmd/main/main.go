d531b84596d3739c6aebe3f2a2d883cf395baf3f
linqcod <linqcod@yandex.ru>
2021-08-25 08:38:42 -0700

    completed-parser

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -1,5 +1,16 @@
 package main
 
+import (
+	"fmt"
+	"russian-dictionary-bot/internal/dict"
+)
+
 func main() {
 
+	c := make(chan *dict.Result)
+	go dict.ParseWordData("манжета", c)
+
+	res := <-c
+	fmt.Println(res)
+	//TODO: add results to database
 }
--- internal/dict/dictionary.go
+++ internal/dict/dictionary.go
@@ -3,8 +3,7 @@ package dict
 import (
 	"fmt"
 	"github.com/gocolly/colly"
-	"io"
-	"net/http"
+	"log"
 	"strings"
 )
 
@@ -33,43 +32,26 @@ type Context struct {
 
 var dictURL string = "https://rustxt.ru/dict/"
 
-func DictionaryAPI(word string) string {
+func ParseWordData(word string, ch chan *Result) {
 
-	var answer string
-
-	if response, err := http.Get(dictURL + strings.ToLower(word)); err != nil {
-		answer = "Dictionary is not respond"
-	} else {
-		defer response.Body.Close()
-
-		//TODO:parse web page and read data
-		//results, err :=
-		ParseWordData(response.Body, word)
-		if err != nil {
-			answer = "Word not found"
-		} else {
-			//TODO: insert word to database
-		}
-	}
-
-	return answer
-}
-
-//(SearchResults, error)
-func ParseWordData(body io.ReadCloser, word string) {
 	c := colly.NewCollector()
 
-	c.OnHTML(".row", func(e *colly.HTMLElement) {
+	c.OnRequest(func(r *colly.Request) {
+		fmt.Printf("Visiting: %s", dictURL+strings.ToLower(word))
+	})
 
+	var result *Result
+
+	c.OnHTML(".col-md-8.col-12", func(e *colly.HTMLElement) {
 		var meanings []Meaning
-		e.ForEach(".p-2", func(i int, e *colly.HTMLElement) {
+		e.ForEach(".answer.border.p-2", func(i int, e *colly.HTMLElement) {
 			var meaning string
 			var author string
 
-			e.ForEach("mb-1", func(i int, e *colly.HTMLElement) {
+			e.ForEach(".mb-1", func(i int, e *colly.HTMLElement) {
 				meaning += e.Text
 			})
-			author = e.ChildText(".mt-1")
+			author = e.ChildText(".d-block.text-right.mb-0.mt-1")
 
 			meanings = append(meanings, Meaning{
 				Meaning: meaning,
@@ -78,25 +60,31 @@ func ParseWordData(body io.ReadCloser, word string) {
 		})
 
 		var synonyms []string
-		e.ForEach("li", func(i int, e *colly.HTMLElement) {
-			synonyms = append(synonyms, e.Text)
-		})
-
-		var contexts []Context
-		e.ForEach(".row .mb-4 .sentence", func(i int, e *colly.HTMLElement) {
-			contexts = append(contexts, Context{
-				Text:   e.ChildText(".sentence-text"),
-				Source: e.ChildText(".text-muted"),
+		e.ForEach(".wrap", func(i int, e *colly.HTMLElement) {
+			e.ForEach("li", func(i int, e *colly.HTMLElement) {
+				synonyms = append(synonyms, e.Text)
 			})
 		})
 
-		result := Result{
+		var contexts []Context
+		e.ForEach(".row.mb-4.sentence", func(i int, e *colly.HTMLElement) {
+			contexts = append(contexts, Context{
+				Text:   e.ChildText(".mb-2.sentence-text"),
+				Source: e.ChildText(".text-muted .text-muted"),
+			})
+		})
+
+		result = &Result{
 			Word:     word,
 			Meanings: meanings,
 			Synonyms: synonyms,
 			Contexts: contexts,
 		}
 
-		fmt.Println(result)
+		ch <- result
 	})
+
+	if err := c.Visit(dictURL + strings.ToLower(word)); err != nil {
+		log.Fatalln("Incorrect request")
+	}
 }
